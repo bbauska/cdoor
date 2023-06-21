70bdfcb57986e06b190d7f0d01540ee6825414de
Brian Bauska <brianbauska@gmail.com>
2023-06-20 22:04:03 -0700

    Update photo-script.js

--- js/photo-script.js
+++ js/photo-script.js
@@ -1,4 +1,14 @@
+var currentImage = document.getElementById("current-image");
+
 var imageThumbs = document.getElementById("image-thumbs");
+thumb.addEventListener(
+  "click", function() {
+
+  }
+);
+
+currentImage.src = this.src;
+currentImage.alt = this.alt;
 
 for (var i = 1; i <= 10; i++) {
 var thumb = document.createElement("img");
