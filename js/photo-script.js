a774c3c48f905e36cf0ddea1dd9407e5684f92fe
Brian Bauska <brianbauska@gmail.com>
2023-06-20 21:59:29 -0700

    Create photo-script.js

---
+++ js/photo-script.js
@@ -0,0 +1,10 @@
+var imageThumbs = document.getElementById("image-thumbs");
+
+for (var i = 1; i <= 10; i++) {
+var thumb = document.createElement("img");
+thumb.src = "images/image" + i + ".jpg";
+thumb.alt = "Image " + i;
+  
+}
+
+thumb.classList.add("thumb");
